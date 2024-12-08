750ee2021fe1cc224acc4bb688659a69ee72748e
Phasyah <phasyahapriliyan151515@gmail.com>
2024-12-08 11:10:44 +0800

    uptade spotifyCard

--- music.js
+++ music.js
@@ -2,7 +2,9 @@ const dataLagu = [
     { title: "Aku Milikmu", artis:"Dewa 19", src: "assets/audio/akumilikmu.mp3" },
     { title: "Terima kasih", artis:"Hal", src: "assets/audio/HAL - terima kasih (Official Lyric Video) - halstage (youtube).mp3" },
     { title: "I Wanna Be Yours", artis:"Arctic Monkeys", src: "assets/audio/I Wanna Be Yours - Arctic Monkeys (youtube).mp3" },
-    { title: "Sempurna", artis:"Andra And The Backbone", src: "assets/audio/Andra And The Backbone - Sempurna (Official Music Video) - GP Records (youtube).mp3" }
+    { title: "Sempurna", artis:"Andra And The Backbone", src: "assets/audio/Andra And The Backbone - Sempurna (Official Music Video) - GP Records (youtube).mp3" },
+    { title: "Resah Jadi Luka", artis:"Daun Jatuh", src: "assets/audio/Daun Jatuh - Resah Jadi Luka (Official Audio) - daunjatuh music (youtube).mp3" },
+    { title: "Spring Snow", artis:"10cm", src: "assets/audio/10cm – Spring Snow (봄눈) [Lovely Runner OST Part 8] [Rom_Eng Lyric] - ElyDha Subs (youtube).mp3" },
 ];
 
 const laguPlayer = document.getElementById("laguPlayer");
@@ -79,3 +81,21 @@ progress.onchange = function(){
     ctrlIcon.classList.add("fa-pause");
     ctrlIcon.classList.remove("fa-play");
 };
+
+
+//Spotify Card
+
+const laguHoney = document.getElementById("boyPablo");
+const playIcon = document.getElementById("playIcon");
+function playSpotify(){
+    if(playIcon.classList.contains("fa-pause")){
+        laguHoney.pause();
+        playIcon.classList.remove("fa-pause");
+        playIcon.classList.add("fa-play");
+    }else{
+        laguHoney.play();
+        playIcon.classList.remove("fa-play");
+        playIcon.classList.add("fa-pause")
+    }
+
+}
