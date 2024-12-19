4c431f300b15a6f0ce2085275a35b5c6132b5a27
Phasyah <phasyahapriliyan151515@gmail.com>
2024-12-19 09:55:40 +0800

    added content

--- music.js
+++ music.js
@@ -1,10 +1,12 @@
 const dataLagu = [
-    { title: "Aku Milikmu", artis:"Dewa 19", src: "assets/audio/akumilikmu.mp3" },
+    { title: "Aku Milikmu", artis:"Dewa 19", src: "assets/audio/spotifydown.com - Aku Milikmu.mp3" },
     { title: "Terima kasih", artis:"Hal", src: "assets/audio/HAL - terima kasih (Official Lyric Video) - halstage (youtube).mp3" },
     { title: "I Wanna Be Yours", artis:"Arctic Monkeys", src: "assets/audio/I Wanna Be Yours - Arctic Monkeys (youtube).mp3" },
     { title: "Sempurna", artis:"Andra And The Backbone", src: "assets/audio/Andra And The Backbone - Sempurna (Official Music Video) - GP Records (youtube).mp3" },
     { title: "Resah Jadi Luka", artis:"Daun Jatuh", src: "assets/audio/Daun Jatuh - Resah Jadi Luka (Official Audio) - daunjatuh music (youtube).mp3" },
     { title: "Spring Snow", artis:"10cm", src: "assets/audio/10cm – Spring Snow (봄눈) [Lovely Runner OST Part 8] [Rom_Eng Lyric] - ElyDha Subs (youtube).mp3" },
+    { title: "I Love You So", artis:"The Walters", src: "assets/audio/spotifydown.com - I Love You So.mp3" },
+    { title: "To The Bone", artis:"Pamungkas", src: "assets/audio/spotifydown.com - To the Bone.mp3" },
 ];
 
 const laguPlayer = document.getElementById("laguPlayer");
